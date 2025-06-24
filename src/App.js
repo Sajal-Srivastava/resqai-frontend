8fd2db0f45fd3e61df9ac720c445ae0715956631
Sajal-Srivastava <144812657+Sajal-Srivastava@users.noreply.github.com>
2025-06-24 18:33:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/classify', {
+      const res = await fetch('https://resqai-backend.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text, location: userLocation }),
